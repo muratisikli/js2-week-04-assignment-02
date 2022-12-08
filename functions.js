a560bdcfeb0f12b5b2dcfa540a60a6ada9677467
muratisikli <muratisikli80@gmail.com>
2022-12-08 16:48:15 +0100

    Added

--- functions.js
+++ functions.js
@@ -1,3 +1,13 @@
-const isTheWordSplit = () => {};
+const isTheWordSplit = (pArray) => {
+    let firstElement=pArray[0];
+    let otherElement=pArray[1].split(",");
+    for (let i=0; i<otherElement.length;i++){
+        for(let j=0; j<otherElement.length; j++){
+            if (otherElement[i]+otherElement[j]===firstElement){
+                return otherElement[i]+ ","+ otherElement[j] }
+        }
+    }
+    return "not possible";
+};
 
 export { isTheWordSplit };
